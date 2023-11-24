9137b3635dc130c5b904e6b288b81a0a9acbb939
Dobbymin <gmin0701@knu.ac.kr>
2023-11-24 17:02:22 +0900

    [Feat]: 재활용가능한 Footer (보류)

---
+++ birthday_funding/src/Layout/Footer.jsx
@@ -0,0 +1,12 @@
+/* import './Footer.scss';
+
+// Footer 안에 들어갈 내용을 props로 받아서 계속 재사용 하고싶은데 어케하는지 모르겠음... 추후 수정
+const Footer = (text) => {
+    return (
+        <>
+            <div className="Footer-container">{text}</div>
+        </>
+    );
+};
+
+export default Footer; */
