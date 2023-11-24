b54125c5707ac5ebc99202a904fd752abe3308ae
Dobbymin <gmin0701@knu.ac.kr>
2023-11-24 21:41:33 +0900

    [Feat]: 회원가입 페이지 작성

--- birthday_funding/src/pages/LoginPage/RegisterPage/RegisterPage.jsx
+++ birthday_funding/src/pages/LoginPage/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import Header from '../../../Layout/Header';
+import './RegisterPage.scss';
 
 const RegisterPage = () => {
-    return <div>RegisterPage</div>;
+    return (
+        <>
+            <Header />
+            <div className="register-container">
+                <h1 className="title">회원가입</h1>
+                <form className="form-container" action="">
+                    <div className="input-container">
+                        {/* <label htmlFor="">ID</label> */}
+                        <input type="text" id="id" placeholder="아이디" className="input-item" />
+                        <div>
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="input-container">
+                        {/* <label htmlFor="">ID</label> */}
+                        <input type="text" id="id" placeholder="비밀번호" className="input-item" />
+                        <div>
+                            <span></span>
+                        </div>
+                    </div>
+                    <div className="button-container">
+                        <button type="submit" className="button-item">
+                            로그인
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 };
 
 export default RegisterPage;
