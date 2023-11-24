a6dc487480e7b3d256d47abb3c45c095c30a6c4d
Dobbymin <gmin0701@knu.ac.kr>
2023-11-25 02:56:25 +0900

    [Fix]: eslint 오류 처리

--- birthday_funding/src/pages/FriendPage/FriendPage.jsx
+++ birthday_funding/src/pages/FriendPage/FriendPage.jsx
@@ -1,71 +1,70 @@
-import React from 'react';
 import Header from '../../Layout/Header';
 import './FriendPage.scss';
 
+/* eslint-disable */
 const friend = [
     {
-        "id" : 1,
-		"birthday" : "2000-01-01",
-		"nickname" : "미정",
-		"userImageUrl" : "../../public/images/intro/gift-box.png",
-        "fundingId" : 1
+        id: 1,
+        birthday: '2000-01-01',
+        nickname: '미정',
+        userImageUrl: '../../public/images/intro/gift-box.png',
+        fundingId: 1,
     },
     {
-        "id" : 2,
-		"birthday" : "2000-01-01",
-		"nickname" : "승민",
-		"userImageUrl" : "../../public/images/intro/gift-box.png",
-        "fundingId" : null
+        id: 2,
+        birthday: '2000-01-01',
+        nickname: '승민',
+        userImageUrl: '../../public/images/intro/gift-box.png',
+        fundingId: null,
     },
     {
-        "id" : 3,
-		"birthday" : "2000-01-01",
-		"nickname" : "홍석",
-		"userImageUrl" : "../../public/images/intro/gift-box.png",
-        "fundingId" : 2
+        id: 3,
+        birthday: '2000-01-01',
+        nickname: '홍석',
+        userImageUrl: '../../public/images/intro/gift-box.png',
+        fundingId: 2,
     },
     {
-        "id" : 4,
-		"birthday" : "2000-01-01",
-		"nickname" : "재현",
-		"userImageUrl" : "../../public/images/intro/gift-box.png",
-        "fundingId" : null
+        id: 4,
+        birthday: '2000-01-01',
+        nickname: '재현',
+        userImageUrl: '../../public/images/intro/gift-box.png',
+        fundingId: null,
     },
     {
-        "id" : 5,
-		"birthday" : "2000-01-01",
-		"nickname" : "강민",
-		"userImageUrl" : "../../public/images/intro/gift-box.png",
-        "fundingId" : null
-    }
-]
+        id: 5,
+        birthday: '2000-01-01',
+        nickname: '강민',
+        userImageUrl: '../../public/images/intro/gift-box.png',
+        fundingId: null,
+    },
+];
 
 const FriendPage = () => {
     return (
         <>
-            <Header msg={'친구 목록'} isMain={false}/>
-            <div className='friend-title'>펀딩 진행 중인 친구</div>
-            <ul className='ul-funding-friend'>
-                {friend.filter((friend) => friend.fundingId !== null)
-                .map(fundingFriend => 
-                    <li id={fundingFriend.id} className="funding-friend-container">
-                        <a href={fundingFriend.fundingId}>
-                            <img className='friend-img' src={fundingFriend.userImageUrl} />
-                            <div className="friend-name">{fundingFriend.nickname}</div>
-                        </a>
-                    </li>
-                )}
-                
+            <Header msg={'친구 목록'} isMain={false} />
+            <div className="friend-title">펀딩 진행 중인 친구</div>
+            <ul className="ul-funding-friend">
+                {friend
+                    .filter((friend) => friend.fundingId !== null)
+                    .map((fundingFriend) => (
+                        <li id={fundingFriend.id} className="funding-friend-container">
+                            <a href={fundingFriend.fundingId}>
+                                <img className="friend-img" src={fundingFriend.userImageUrl} />
+                                <div className="friend-name">{fundingFriend.nickname}</div>
+                            </a>
+                        </li>
+                    ))}
             </ul>
-            <div className='friend-title'>친구 목록</div>
-            <ul className='ul-friend'>
-                {friend.map(friend =>
+            <div className="friend-title">친구 목록</div>
+            <ul className="ul-friend">
+                {friend.map((friend) => (
                     <li id={friend.id} className="friend-container">
-                        <img className='friend-img' src={friend.userImageUrl} />
+                        <img className="friend-img" src={friend.userImageUrl} />
                         <div className="friend-name">{friend.nickname}</div>
                     </li>
-                )}
-                
+                ))}
             </ul>
         </>
     );
