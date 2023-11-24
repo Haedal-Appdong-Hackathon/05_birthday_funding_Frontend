f40cfa776ed19127b9bcae8d3afc68b8dd031c83
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2023-11-25 03:22:16 +0900

    [Feat] funding progress page

--- birthday_funding/src/pages/FundingPage/ProgressFunding/ProgressFunding.jsx
+++ birthday_funding/src/pages/FundingPage/ProgressFunding/ProgressFunding.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import Header from '../../../Layout/Header';
+import './ProgressFunding.scss'
+
+const funding = {
+	"id" : 1,
+	"title" : "펀딩제목",
+	"startDate" : "2023-11-1",
+	"endDate" : "2023-11-15",
+	"progress" : 80.0,
+	"writer" : {
+		"id" : 1,
+		"birthday" : "2000-01-01",
+		"nickname" : "도비민민",
+		"userImageUrl" : "aa.xxx.png"
+	},
+
+
+	"content" : "에어팟을 주세요",
+	"currentMoney" : 8000,
+	"wishList" : [
+		{
+			"id" : 1,
+			"name" : "에어팟",
+			"brand" : "스타벅스",
+			"price" : 10000,
+			"imageUrl" : "../../../public/images/intro/gift-box.png",
+		}
+	]
+
+}
 
 const ProgressFunding = () => {
-    return <div>ProgressFunding</div>;
+    console.log(funding)
+    return (
+        <>
+            <Header msg={'펀딩하기'} isMain={false}/>
+            <div className='funding-container'>
+                <div className='funding-day'>{funding.endDate} 까지</div>
+                <img className='funding-img' src={funding.wishList[0].imageUrl}/>
+                <div className='funding-progress'>
+                    <div>{funding.progress}%</div>
+                    <progress value={funding.progress} max="100"></progress>
+                </div>
+                <div className='funding-title'>{funding.title}</div>
+                <div className='funding-user-name'>{funding.writer.nickname}의 펀딩</div>
+                <div className='funding-content'>{funding.content}</div>
+            </div>
+        </>
+    );
 };
 
 export default ProgressFunding;
