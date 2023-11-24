a19c3e629a5a2843e637facbcd95b0b3d63e418a
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2023-11-25 02:45:43 +0900

    [Feat] Store page

--- birthday_funding/src/pages/FriendPage/FriendPage.jsx
+++ birthday_funding/src/pages/FriendPage/FriendPage.jsx
@@ -44,11 +44,11 @@ const FriendPage = () => {
     return (
         <>
             <Header msg={'친구 목록'} isMain={false}/>
-            <div className='friend-title'>펀딩 진행 중인 친구</div>
+            <div className='title'>펀딩 진행 중인 친구</div>
             <ul className='ul-funding-friend'>
                 {friend.filter((friend) => friend.fundingId !== null)
                 .map(fundingFriend => 
-                    <li id={fundingFriend.id} className="funding-friend-container">
+                    <li key={fundingFriend.id} className="funding-friend-container">
                         <a href={fundingFriend.fundingId}>
                             <img className='friend-img' src={fundingFriend.userImageUrl} />
                             <div className="friend-name">{fundingFriend.nickname}</div>
@@ -57,10 +57,10 @@ const FriendPage = () => {
                 )}
                 
             </ul>
-            <div className='friend-title'>친구 목록</div>
+            <div className='title'>친구 목록</div>
             <ul className='ul-friend'>
                 {friend.map(friend =>
-                    <li id={friend.id} className="friend-container">
+                    <li key={friend.id} className="friend-container">
                         <img className='friend-img' src={friend.userImageUrl} />
                         <div className="friend-name">{friend.nickname}</div>
                     </li>
