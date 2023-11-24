e23115f695e1e4521ab3e5767f29f7d4216e619b
Seung Min Lee <74231194+miloul@users.noreply.github.com>
2023-11-24 21:24:47 +0900

    [Design]: three js 추가

--- birthday_funding/src/pages/IntroPage/IntroPage.jsx
+++ birthday_funding/src/pages/IntroPage/IntroPage.jsx
@@ -1,21 +1,27 @@
+import { Canvas } from '@react-three/fiber';
 import Header from '../../Layout/Header';
 // import Footer from '../../Layout/footer';
+import { Html, OrbitControls } from '@react-three/drei';
 import './IntroPage.scss';
+import Gift from './Gift';
 
 const IntroPage = () => {
     return (
         <>
             <Header />
-            <div className="intro-container">
-                <img src="../../../public/images/intro/gift-box.png" alt="cake" />
-                <text>
+            <Canvas>
+                <ambientLight intensity={10} />
+                <Gift />
+            </Canvas>
+            <div className="intro-container">    
+                <p>
                     Birthday Funding은 <br />
                     축하하는 마음을 모아 <br />
                     다함께 선물하는 서비스입니다. <br />
                     <br />
                     가격이 부담되어 망설였던 선물도 <br />
                     펀딩을 통해 선물로 받아보세요! <br />
-                </text>
+                </p>
             </div>
             <div className="Footer-container">로그인하고 시작하기</div>
         </>
