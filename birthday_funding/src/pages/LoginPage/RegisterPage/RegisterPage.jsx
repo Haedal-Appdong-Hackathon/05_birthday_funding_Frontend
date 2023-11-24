2dbedcc960dcbbd314bd697fbd5c8eef7d82ce48
Dobbymin <gmin0701@knu.ac.kr>
2023-11-25 02:14:38 +0900

    [Feat]: 회원가입 페이지 구현

--- birthday_funding/src/pages/LoginPage/RegisterPage/RegisterPage.jsx
+++ birthday_funding/src/pages/LoginPage/RegisterPage/RegisterPage.jsx
@@ -9,22 +9,22 @@ const RegisterPage = () => {
                 <h1 className="title">회원가입</h1>
                 <form className="form-container" action="">
                     <div className="input-container">
-                        {/* <label htmlFor="">ID</label> */}
-                        <input type="text" id="id" placeholder="아이디" className="input-item" />
+                        <label htmlFor="">이름</label>
+                        <input type="text" id="name" placeholder="이름" className="input-item" />
                         <div>
                             <span></span>
                         </div>
                     </div>
                     <div className="input-container">
-                        {/* <label htmlFor="">ID</label> */}
-                        <input type="text" id="id" placeholder="비밀번호" className="input-item" />
+                        <label htmlFor="">생년월일</label>
+                        <input type="number" id="birth" placeholder="ex) 2023-11-25" className="input-item" />
                         <div>
                             <span></span>
                         </div>
                     </div>
                     <div className="button-container">
                         <button type="submit" className="button-item">
-                            로그인
+                            회원가입하기
                         </button>
                     </div>
                 </form>
