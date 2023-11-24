0814e694b2ef2a7835adbd849ab4c97b095e71d1
김강민 <79429482+Dobbymin@users.noreply.github.com>
2023-11-24 22:48:21 +0900

    Revert "로그인페이지"

--- birthday_funding/src/pages/LoginPage/LoginPage.jsx
+++ birthday_funding/src/pages/LoginPage/LoginPage.jsx
@@ -1,27 +1,7 @@
-import Header from '../../Layout/Header';
-import './LoginPage.scss';
+import React from 'react';
 
 const LoginPage = () => {
-    return (
-        <>
-            <Header />
-            <div className="login-container">
-                <img className="cake-item" src="../../../public/images/login/cake-3D.png" alt="cake" />
-                <p>
-                    카카오 로그인으로
-                    <br />
-                    간편하게 펀딩을 참여하세요!
-                    <br />
-                </p>
-
-                <img
-                    className="kakao-button"
-                    src="../../../public/images/login/kakao_login_large_wide.png"
-                    alt="kakao-button"
-                />
-            </div>
-        </>
-    );
+    return <div>LoginPage</div>;
 };
 
 export default LoginPage;
